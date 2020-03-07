0df4e3d64f6ebd9d6a253ca26ae7f0a51a6386ff
James N. V. Cash <james.nvc@gmail.com>
2020-03-07 17:06:39 -0500

    Handle module-qualified DCGs

--- grammar.js
+++ grammar.js
@@ -27,6 +27,7 @@ module.exports = grammar({
       "."),
 
     dcg_definition: $ => seq(
+      optional(seq(field('module', $.atom), ':')),
       field('head', choice($.atom, $.term)),
       optional(seq(',', field('semicontext', $.list))),
       choice('-->', '-->>'),
